45f7947cbb10def29b1d33d8f85b06affa5ee516
senshsk <felipe.blasquez@sansano.usm.cl>
2022-09-20 03:18:03 -0300

    Update backspace.cpp

--- VJudge/backspace.cpp
+++ VJudge/backspace.cpp
@@ -1,4 +1,6 @@
 #include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
@@ -10,9 +12,10 @@ int main(){
     int count = 0;
     int len_x = x.size();
     while (count<len_x){
-        if (x[count]=="<"){
-            x-=x[count-1];
-            x-=x[count-1];
+        if ("<" in x){
+            
+            x.erase(count-1, 0);
+            x.erase(count-1, 0);
             count-=2;
         }
         count+=1;
