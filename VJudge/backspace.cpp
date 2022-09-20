c0bc3d07f2abe493134bbe2d92d54460425f6c77
senshsk <felipe.blasquez@sansano.usm.cl>
2022-09-20 02:30:05 -0300

    Revert "Delete backspace.cpp"
    This reverts commit 9771ee0e34d5aefa03b45f7c73791b0eb0bfbdc6.

---
+++ VJudge/backspace.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string x;
+    cin>>x;
+    t=true;
+    int count = 0;
+    int len_x = x.size();
+    while count<len_x{
+        if (x[count]=="<"){
+            x-=x[count-1];
+            x-=x[count-1];
+            count-=2;
+        }
+        count+=1;
+    }
+    return 0;
+}
